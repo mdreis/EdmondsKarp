33af8d8aed501b745cf9bd8097774fb7a78b509f
Jonathan Williams <jcwilliams37@crimson.ua.edu>
2024-04-15 18:08:53 -0400

    Remove unnecessary code comment

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@
 # Created: Monday, April 15th 2024 at 11:47:8                                  #
 # Author: Jonathan Williams                                                    #
 # -----                                                                        #
-# Last Modified: Monday, April 15th 2024 17:06:21                              #
+# Last Modified: Monday, April 15th 2024 17:08:18                              #
 # Modified By: Jonathan Williams                                               #
 ###############################################################################
 
@@ -50,32 +50,6 @@ def bfs(graph: nx.DiGraph, source, sink, parent):
                 if v == sink:
                     return True
     return False
-
-
-# int bfs(int s, int t, vector<int>& parent) {
-#     fill(parent.begin(), parent.end(), -1);
-#     parent[s] = -2;
-#     queue<pair<int, int>> q;
-#     q.push({s, INF});
-
-#     while (!q.empty()) {
-#         int cur = q.front().first;
-#         int flow = q.front().second;
-#         q.pop();
-
-#         for (int next : adj[cur]) {
-#             if (parent[next] == -1 && capacity[cur][next]) {
-#                 parent[next] = cur;
-#                 int new_flow = min(flow, capacity[cur][next]);
-#                 if (next == t)
-#                     return new_flow;
-#                 q.push({next, new_flow});
-#             }
-#         }
-#     }
-
-#     return 0;
-# }
 
 
 # Function to calculate the maximum flow using Edmonds-Karp algorithm (BFS based)
